a799134f14102a34b1e764f6c7bbf5775112cf63
MatiasRojasLemus <mrojaslemu1@educacion.navarra.es>
2024-04-22 17:09:54 +0200

    Código Clase Trabajador (Constructor, Getters y Setters)

--- src/main/java/org/example/gestorempleadosmatiasrojas/Trabajador.java
+++ src/main/java/org/example/gestorempleadosmatiasrojas/Trabajador.java
@@ -1,4 +1,41 @@
 package org.example.gestorempleadosmatiasrojas;
 
+import java.time.LocalDate;
+
 public class Trabajador {
+    private String nombre;
+    private String puesto;
+    private int salario;
+    private LocalDate fechaAlta;
+
+    public Trabajador(String nombre, String puesto, int salario, LocalDate fechaAlta) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;
+        this.fechaAlta = fechaAlta;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getPuesto() {
+        return puesto;
+    }
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+    public int getSalario() {
+        return salario;
+    }
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+    public LocalDate getFechaAlta() {
+        return fechaAlta;
+    }
+    public void setFechaAlta(LocalDate fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
 }
