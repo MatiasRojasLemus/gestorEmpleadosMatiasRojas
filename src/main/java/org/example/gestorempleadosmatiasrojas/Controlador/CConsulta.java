c9d8913fb1b9a42d37685322ee60eea528918082
MatiasRojasLemus <mrojaslemu1@educacion.navarra.es>
2024-04-29 03:26:08 +0200

    Modificaciones en PInserccion y CInserccion. Creacion y estructuración de PConsulta y creación de CConsulta.

---
+++ src/main/java/org/example/gestorempleadosmatiasrojas/Controlador/CConsulta.java
@@ -0,0 +1,37 @@
+package org.example.gestorempleadosmatiasrojas.Controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.text.Text;
+
+public class CConsulta {
+    @FXML
+    private Text txtTit2;
+    @FXML
+    private TabPane TabP2;
+    @FXML
+    private Tab tabNE2;
+    @FXML
+    private Tab TabCon2;
+    @FXML
+    public ListView listV;
+    @FXML
+    private Button btnEdit;
+    @FXML
+    private Button btnRefresc;
+    @FXML
+    private Button btnElim;
+    @FXML
+    private Text txtIdT;
+    @FXML
+    private Text txtNT;
+    @FXML
+    private Text txtPuesto;
+    @FXML
+    private Text txtSalar;
+    @FXML
+    private Text txtFA;
+}
--- src/main/java/org/example/gestorempleadosmatiasrojas/Controlador/CInserccion.java
+++ src/main/java/org/example/gestorempleadosmatiasrojas/Controlador/CInserccion.java
@@ -1,20 +1,32 @@
 package org.example.gestorempleadosmatiasrojas.Controlador;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 public class CInserccion {
-    public static final TabPane TP = new TabPane();
-    public VBox VBoxInsert;
-    public Text txtTitApp;
-    public Tab tabNE;
-    public Tab tabCon;
-    public Label lblNom;
-    public TextField txtFldNom;
-    public Label lblPuesto;
-    public ComboBox CBPuesto;
-    public Label lblSalario;
-    public TextField txtFldSalario;
-    public Button btnInsert;
+    private static final TabPane TP = new TabPane();
+    @FXML
+    private VBox VBoxInsert;
+    @FXML
+    private Text txtTitApp;
+    @FXML
+    private Tab tabNE;
+    @FXML
+    private Tab tabCon;
+    @FXML
+    private Label lblNom;
+    @FXML
+    private TextField txtFldNom;
+    @FXML
+    private Label lblPuesto;
+    @FXML
+    private ComboBox CBPuesto;
+    @FXML
+    private Label lblSalario;
+    @FXML
+    private TextField txtFldSalario;
+    @FXML
+    private Button btnInsert;
 }
