3e0505793f7d20868342d85ed2f4258295cea9e6
MatiasRojasLemus <mrojaslemu1@educacion.navarra.es>
2024-05-05 23:15:27 +0200

    Clase Trabajador: Eliminación de setters innecesarios.

--- src/main/java/Modelo/Trabajador.java
+++ src/main/java/Modelo/Trabajador.java
@@ -17,25 +17,13 @@ public class Trabajador {
     public String getNombre() {
         return nombre;
     }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
     public String getPuesto() {
         return puesto;
-    }
-    public void setPuesto(String puesto) {
-        this.puesto = puesto;
     }
     public int getSalario() {
         return salario;
     }
-    public void setSalario(int salario) {
-        this.salario = salario;
-    }
     public Date getFechaAlta() {
         return fechaAlta;
-    }
-    public void setFechaAlta(Date fechaAlta) {
-        this.fechaAlta = fechaAlta;
     }
 }
